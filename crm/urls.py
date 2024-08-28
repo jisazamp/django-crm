292d572746dede038fdac6217e0c583c0f082a3f
Juan Pablo Isaza M. <jisaza@vaovatravel.com>
2024-08-27 20:51:49 -0500

    login

--- crm/urls.py
+++ crm/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('', views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("logout/", views.logout_user, name="logout"),
+]
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
-# Create your views here.
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect("home")
+        else:
+            messages.error(request, "Something went wrong when trying to sign you in")
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+
+def logout_user(request):
+    pass
